cfe2ee5c814c7970cc4e7e5eba3f3b652de5050c
louis-25 <20151577@vision.hoseo.edu>
2021-04-29 23:04:08 +0900

    Navbar, app

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,59 @@
-import './app.css';
-import Habits from './components/habits';
+import './app.css'
+import Habits from './components/habits'
+import Navbar from './components/Navbar'
+import React, { Component } from 'react'
 
-function App() {
-  return <Habits></Habits>;
+class App extends Component {
+    state = {
+        habits: [
+            //state에 habits 배열생성
+            { id: 1, name: 'Reading', count: 1 },
+            { id: 2, name: 'Reading', count: 2 },
+            { id: 3, name: 'Reading', count: 3 },
+        ],
+    }
+    handleIncrement = habit => {
+        const habits = [...this.state.habits]
+        const index = habits.indexOf(habit)
+        habits[index].count++
+        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+    }
+
+    handleDecrement = habit => {
+        const habits = [...this.state.habits]
+        const index = habits.indexOf(habit)
+        const count = habits[index].count - 1
+        habits[index].count = count < 0 ? 0 : count
+        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+    }
+
+    handleDelete = habit => {
+        /*const habits = [...this.state.habits]
+        const index = habits.indexOf(habit)
+        habits.splice(index, 1)*/
+        //filter는 새로운배열을 만들어준다
+        const habits = this.state.habits.filter(item => item.id !== habit.id)
+        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+    }
+    render() {
+        return (
+            <>
+                <Navbar
+                    totalCount={
+                        //count가 0이상인 habits배열의 길이
+                        this.state.habits.filter(item => item.count > 0).length
+                    }
+                ></Navbar>
+                <Habits
+                    habits={this.state.habits}
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                    onDelete={this.handleDelete}
+                ></Habits>
+                ;
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -2,40 +2,21 @@ import React, { Component } from 'react'
 import Habit from './habit'
 
 class habits extends Component {
-    state = {
-        habits: [
-            //state에 habits 배열생성
-            { id: 1, name: 'Reading', count: 1 },
-            { id: 2, name: 'Reading', count: 2 },
-            { id: 3, name: 'Reading', count: 3 },
-        ],
-    }
     handleIncrement = habit => {
-        const habits = [...this.state.habits]
-        const index = habits.indexOf(habit)
-        habits[index].count++
-        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+        this.props.onIncrement(habit)
     }
 
     handleDecrement = habit => {
-        const habits = [...this.state.habits]
-        const index = habits.indexOf(habit)
-        const count = habits[index].count - 1
-        habits[index].count = count < 0 ? 0 : count
-        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+        this.props.onDecrement(habit)
     }
 
     handleDelete = habit => {
-        /*const habits = [...this.state.habits]
-        const index = habits.indexOf(habit)
-        habits.splice(index, 1)*/
-        const habits = this.state.habits.filter(item => item.id !== habit.id)
-        this.setState({ habits: habits }) //그냥 habits만 써줘도 같은동작
+        this.props.onDelete(habit)
     }
     render() {
         return (
             <ul>
-                {this.state.habits.map(habit => (
+                {this.props.habits.map(habit => (
                     //Habit컴포넌트에 habit이라는 props를 전달
                     //Habit에서는 비구조화 할당을하여 사용하자
                     <Habit
