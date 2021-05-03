a0c24301921c7eeaece615adf7751dd2a6ae719e
louis-25 <20151577@vision.hoseo.edu>
2021-05-03 14:18:25 +0900

    template

--- habit-tracker/src/components/HabitAddForm.jsx
+++ habit-tracker/src/components/HabitAddForm.jsx
@@ -1,28 +1,26 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 
-class HabitAddForm extends PureComponent {
-    inputRef = React.createRef()
+const HabitAddForm = memo(props => {
+    const inputRef = React.createRef()
+    const formRef = React.createRef()
 
-    onSubmit = event => {
+    const onSubmit = event => {
         event.preventDefault()
         const name = this.inputRef.current.value
         name && this.props.onAdd(name)
-        this.inputRef.current.value = ''
+        formRef.current.reset()
     }
-    render() {
-        console.log('addform')
-        return (
-            <form className="add-form" onSubmit={this.onSubmit}>
-                <input
-                    ref={this.inputRef}
-                    type="text"
-                    className="add-input"
-                    placeholder="Habit"
-                />
-                <button className="add-button">Add</button>
-            </form>
-        )
-    }
-}
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input
+                ref={inputRef}
+                type="text"
+                className="add-input"
+                placeholder="Habit"
+            />
+            <button className="add-button">Add</button>
+        </form>
+    )
+})
 
 export default HabitAddForm
