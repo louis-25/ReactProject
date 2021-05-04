788630ac0ab9830bcac9b167f55568ba46d851e5
louis-25 <20151577@vision.hoseo.edu>
2021-05-05 01:30:58 +0900

    search

--- youtube-api/src/app.jsx
+++ youtube-api/src/app.jsx
@@ -5,8 +5,24 @@ import VideoList from './components/video_list/video_list'
 
 function App() {
     const [videos, setVideos] = useState([])
+    const search = query => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+        }
+
+        fetch(
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9pVVONrr9-7s3Mo1rKOujWdoPk2rX5FU`,
+            requestOptions
+        )
+            .then(response => response.json())
+            .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+            .then(items => setVideos(items))
+            .catch(error => console.log('error', error))
+    }
 
     useEffect(() => {
+        /* 맨 처음 mount됐을때 한번 호출 */
         const requestOptions = {
             method: 'GET',
             redirect: 'follow',
@@ -20,9 +36,10 @@ function App() {
             .then(result => setVideos(result.items))
             .catch(error => console.log('error', error))
     }, [])
+
     return (
-        <div className={styles.app}>
-            <Search_Header></Search_Header>
+        <div className={styles.app}>            
+            <Search_Header onSearch={search}/>
             <VideoList videos={videos} />
         </div>
     )
--- youtube-api/src/components/search_header/search_header.jsx
+++ youtube-api/src/components/search_header/search_header.jsx
@@ -1,16 +1,40 @@
 import styles from './search_header.module.css'
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Search_Header = props => {
+const Search_Header = ({onSearch}) => { /*onSearch는 app에서 보낸 props*/
+    const inputRef = useRef() //input을 가리켜 내용을 가져올 수 있다
+    const handleSearch = () => {
+        const value = inputRef.current.value; /*input의 값 긁어오기*/
+        onSearch(value)
+    }
+    const onClick = () => {
+        handleSearch();
+    }
+
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter') {
+            handleSearch();
+        }
+    }
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-            <img className={styles.img} src="/images/logo.png" alt="logo" />
-            <h1 className={styles.title}>Youtube</h1>
+                <img className={styles.img} src="/images/logo.png" alt="logo" />
+                <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input className={styles.input} type="search" placeholder="Search..." />
-            <button className={styles.button} type="submit">
-                <img className={styles.buttonImg} src="/images/search.png" alt="search" />
+            <input
+                ref={inputRef}
+                className={styles.input}
+                type="search"
+                placeholder="Search..."
+                onKeyPress={onKeyPress}
+            />
+            <button className={styles.button} type="submit" onClick={onClick}>
+                <img
+                    className={styles.buttonImg}
+                    src="/images/search.png"
+                    alt="search"
+                />
             </button>
         </header>
     )
