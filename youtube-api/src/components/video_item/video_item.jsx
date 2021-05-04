d63e33069373c8d0da960d30eddb21f2f26377fa
louis-25 <20151577@vision.hoseo.edu>
2021-05-04 21:32:22 +0900

    make video item

--- youtube-api/src/components/video_item/video_item.jsx
+++ youtube-api/src/components/video_item/video_item.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from 'react'
+import styles from './video_item.module.css'
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = props => (
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img
+                className={styles.thumbnail}
+                src={props.video.snippet.thumbnails.medium.url}
+                alt="video"
+            />
+            <div className={styles.metadata}>
+                <p className={styles.title}>{props.video.snippet.title}</p>
+                <p className={styles.channel}>
+                    {props.video.snippet.channelTitle}
+                </p>
+            </div>
+        </div>
+    </li>
+)
 
-export default VideoItem;
+export default VideoItem
--- youtube-api/src/components/video_list/video_list.jsx
+++ youtube-api/src/components/video_list/video_list.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import VideoItem from '../video_item/video_item';
+import React from 'react'
+import VideoItem from '../video_item/video_item'
+import styles from './video_list.module.css'
 
 const VideoList = props => (
-  <ul>
-    {props.videos.map(video => (
-      <VideoItem key={video.id} video={video} />
-    ))}
-  </ul>
-);
+    <ul className={styles.videos}>
+        {props.videos.map(video => (
+            //여러개의 요소를 전달할때는 id값이 필요하다
+            <VideoItem key={video.id} video={video} />
+        ))}
+    </ul>
+)
 
-export default VideoList;
+export default VideoList
