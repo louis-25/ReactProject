d49c7d213aefefa292c411d8f32d1f0ca298fbec
louis-25 <20151577@vision.hoseo.edu>
2021-05-20 00:52:07 +0900

    editor preview

--- card-maker/src/components/maker/maker.jsx
+++ card-maker/src/components/maker/maker.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
+import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Preview from '../preview/preview';
 import styles from './maker.module.css'
 
 
@@ -23,6 +25,10 @@ const Maker = ({authService}) => {
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
+            <div className={styles.container}>
+                <Editor/>
+                <Preview/>
+            </div>
             <Footer/>
         </section>
     )
