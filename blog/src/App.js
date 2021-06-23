0e7d4778251f9cde374b68b730f553ddc28b11a9
louis-25 <20151577@vision.hoseo.edu>
2021-06-23 22:58:48 +0900

    이벤트 리스너

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,13 @@
-//import logo from './logo.svg';
+/* eslint-disable */
+import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+    
   let posts = '맛집';
-  let [글제목, 글제목변경] = useState('맛집 추천');
-
+  let [글제목, 글제목변경] = useState(['BBQ', '교촌치킨', '푸라닭']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (    
     <div className="App">
@@ -14,7 +15,17 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h4> { 글제목 } </h4>
+        <h4> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+        <p>6월 23일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[1] } </h4>
+        <p>6월 23일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[2] } </h4>
         <p>6월 23일 발행</p>
         <hr/>
       </div>      
