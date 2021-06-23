834efd8a3d803e922bbf205f8f7af26658903ce5
louis-25 <20151577@vision.hoseo.edu>
2021-06-23 16:35:28 +0900

    useState

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   
   let posts = '맛집';
-  // function 함수() {
-  //   return 100
-  // }
+  let [글제목, 글제목변경] = useState('맛집 추천');
+
+
   return (    
     <div className="App">
       <div className="black-nav">
-        <div style={{color:'blue', fontSize:'30px'}}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={logo}></img>
-      <h4> {posts} </h4>
+      <div className="list">
+        <h4> { 글제목 } </h4>
+        <p>6월 23일 발행</p>
+        <hr/>
+      </div>      
     </div>
   );
 }
