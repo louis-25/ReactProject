4f181975a99db2fc47a33ff2387a9d565a3dc4b8
louis-25 <20151577@vision.hoseo.edu>
2021-08-06 15:15:09 +0900

    Google, Github 로그인

--- neogulweb/src/index.js
+++ neogulweb/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // 라우터
+import AuthService from './service/auth_service' // 파이어베이스 인증
+
+const authService = new AuthService()
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App authService={authService}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
