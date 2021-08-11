16589e87e5c5db5fa4153d56a79abbcc71b676cc
louis-25 <20151577@vision.hoseo.edu>
2021-08-11 17:11:31 +0900

    pull에러 해결

--- neogulweb/src/components/Main/Container/Post/Post.jsx
+++ neogulweb/src/components/Main/Container/Post/Post.jsx
@@ -11,10 +11,10 @@ function Post({post, user, deletePost}) {
       <div className={style.header}>
         <div className={style.profile} style={{backgroundImage:`url(${post.userImage})`}}></div>
         <div className={style.textBox}>
-
-          {post.name ===  user.displayName?
+        <span className={style.name}>{post.name}</span>
+          {/* {post.name ===  user.displayName?
           <span className={style.name}>{post.name}<div onClick={()=>{deletePost(post)}} className={style.trash}><FontAwesomeIcon icon={faTrashAlt}/></div></span>
-          :<span className={style.name}>{post.name}</span>}
+          :<span className={style.name}>{post.name}</span>} */}
 
           <strong><p className={style.contentText}>{post.content}</p></strong>
           <p className={style.date}>{post.date}</p>
