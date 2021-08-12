619769e4dc27d457b5aee374cf3a07015eba47b5
louis-25 <20151577@vision.hoseo.edu>
2021-08-12 17:59:06 +0900

    전송버튼 에러해결

--- neogulweb/src/components/Main/Container/Input/Input.jsx
+++ neogulweb/src/components/Main/Container/Input/Input.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useRef } from 'react';
 import style from './Input.module.css'
 
 function Input({createOrUpdateCard, user, posts}) {
   let today = new Date();
+  const inputText = useRef();
   let post = {
     no: Object.keys(posts).length, //게시물 길이
     id: user.uid,
@@ -26,14 +28,16 @@ function Input({createOrUpdateCard, user, posts}) {
       alert('값을 입력해주세요')
     }else{
       console.log('click post ',post)
-      let no = Object.keys(posts).length
-      createOrUpdateCard(post, no)
+      // let no = Object.keys(posts).length
+      createOrUpdateCard(post)
+      inputText.current.value=""
+      inputText.current.focus()
     }
   }
 
   return (
     <div className={style.inputBox}>
-      <span className={style.formInput}><input onKeyPress={(e)=>{if(e.key === 'Enter'){send(); e.target.value=""}}} className="form-control" type="text" onChange={handleChange}/></span>
+      <span className={style.formInput}><input ref={inputText} onKeyPress={(e)=>{if(e.key === 'Enter'){send(); e.target.value=""}}} className="form-control" type="text" onChange={handleChange}/></span>
       <button onClick={send} className="btn btn-primary" post={post}>전송</button>
     </div>
   );
