1f03d77d3cbb91b098cb28413c05b57521ac6cdc
louis-25 <20151577@vision.hoseo.edu>
2021-09-24 13:27:10 +0900

    work 카테고리

--- portfolio/src/components/navbar/navbar.jsx
+++ portfolio/src/components/navbar/navbar.jsx
@@ -1,52 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import style from './navbar.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAppleAlt, faBars } from "@fortawesome/free-solid-svg-icons";
+import classNames from 'classnames';
 
 function Navbar({setScrollY}) {       
 
-    const Home = () => {
+    const [toggle, setToggle] = useState(false)
+
+    const goHome = () => {
         // console.log(window.pageYOffset)
         setScrollY(0)
     }
 
-    const About = () => {
+    const goAbout = () => {
         setScrollY(567)
     }
 
-    const Skills = () => {
+    const goSkills = () => {
         setScrollY(1200)
     }
 
-    const Work = () => {
+    const goWork = () => {
         setScrollY(2866)
     }
 
-    const Contact = () => {
+    const goContact = () => {
         setScrollY(3658)
+    }    
+
+    const onToggle = () => {
+        setToggle(!toggle)
+        console.log(toggle)
     }
 
     return (
         <>
-    <nav className={style.navbar}>
-        <div className={style.logo}>
-            {/* <i className="fas fa-at"></i> */}
-            <Icon></Icon>
+    <nav className={classNames(style.navbar)}>
+        <div className={style.logo}>            
+            <Icon className={style.icon}></Icon>
             <a href="/">Louis</a>
         </div>
-        <div className={style.menu}>
-            <ul className={style.menu}>
-                {/* data-link는 section id를 불러온다 */}
-                <li className={style.item} onClick={Home}>Home</li>
-                <li className={style.item} onClick={About}>About</li>
-                <li className={style.item} onClick={Skills}>Skills</li>
-                <li className={style.item} onClick={Work}>My work</li>                
-                <li className={style.item} onClick={Contact}>Contact</li>
-            </ul>
+        <div className={style.menu}>            
+            <ul className={classNames(style.menu, toggle ? style.open : style.close)}>
+                {/* data-link는 section id를 불러온다 */}                
+                <li className={style.item} onClick={goHome}>Home</li>
+                <li className={style.item} onClick={goAbout}>About</li>
+                <li className={style.item} onClick={goSkills}>Skills</li>
+                <li className={style.item} onClick={goWork}>My work</li>                
+                <li className={style.item} onClick={goContact}>Contact</li>                
+            </ul>            
         </div>
-        {/* Toggle button - 햄버거 */}
-        {/* <button className={`${styles.navbar__toggle_btn}`}>
-            <i className="fas fa-bars"></i>
-        </button> */}
+        {/* Toggle button - 햄버거 */}        
+        <button className={`${style.toggle_btn}`} onClick={onToggle}>
+            <FontAwesomeIcon icon={faBars} />
+        </button>
     </nav>
     </>
 )
@@ -55,10 +63,9 @@ function Navbar({setScrollY}) {
 function Icon() {
     return(
         <div>
-            <FontAwesomeIcon icon={["fab", "apple"]} />
+            <FontAwesomeIcon icon={faAppleAlt} />
         </div>
     )
-
 }
 
 export default Navbar;
--- portfolio/src/components/work/work.jsx
+++ portfolio/src/components/work/work.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './work.module.css'
 import DHtube from '../../imgs/jobs/DHtube.PNG'
 import corona from '../../imgs/jobs/114_logo.png'
@@ -7,167 +7,128 @@ import github from '../../imgs/jobs/github_clone.PNG'
 import Django from '../../imgs/jobs/Django.png'
 import 자취꿀팁 from '../../imgs/jobs/자취꿀팁.png'
 import 너굴앱 from '../../imgs/jobs/너굴앱.png'
+import classNames from 'classnames'
 
 function Work(props) {
+  const [selectBtn, setSelectBtn] = useState('all')
+  const works = [
+    {
+      href:'https://dhtube.netlify.app/',
+      datatype: 'front-end',
+      src: DHtube,
+      h3: 'React',
+      span: 'DHtube'
+    },
+    {
+      href:'http://124.5.187.217:9090/',
+      datatype: 'back-end',
+      src: corona,
+      h3: 'Spring Boot',
+      span: 'Corona-114'
+    },
+    {
+      href:'https://github.com/louis-25/FinalProject-raspberry',
+      datatype: 'back-end',
+      src: "https://github.com/louis-25/FinalProject-raspberry/raw/main/md-images/image-20210615104043936.png",
+      h3: 'Python',
+      span: 'GiGA Genie'
+    },
+    {
+      href:'https://louis-25.github.io/React.js_Study/',
+      datatype: 'front-end',
+      src: TodoList,
+      h3: 'React.js',
+      span: 'TodoList'
+    },
+    {
+      href:'https://louis-25.github.io/Clone_Coding/',
+      datatype: 'front-end',
+      src: github,
+      h3: 'HTML',
+      span: 'GitHub clone-coding'
+    },
+    {
+      href:'https://github.com/louis-25/Django_Practice/tree/django_shopping',
+      datatype: 'back-end',
+      src: Django,
+      h3: 'Django',
+      span: '쇼핑몰 관리자페이지'
+    },
+    {
+      href:'https://github.com/louis-25/TIL/tree/main/spring/SemiProject',
+      datatype: 'back-end',
+      src: 자취꿀팁,
+      h3: 'Spring',
+      span: '게시판'
+    },
+    {
+      href:'https://drive.google.com/file/d/1SC4T2m8gadtkg33AoS_5UI04_95urheK/view?usp=sharing',
+      datatype: 'mobile',
+      src: 너굴앱,
+      h3: 'Android',
+      span: '너굴앱'
+    },
+  ]
+
   return (
     <section className={style.section}>
-            <h1>My work</h1>
-            <h3>Projects</h3>
-            <div className={style.work__categories}>
-                <button className={`${style.category__btn} ${style.selected}`}>
-                    All <span className={style.category__count}>8</span>
-                </button>
-                <button className={style.category__btn}>
-                    Front-end <span className={style.category__count}>3</span>
-                </button>
-                <button className={style.category__btn}>
-                    Back-end <span className={style.category__count}>4</span>
-                </button>
-                <button className={style.category__btn}>
-                    Mobile <span className={style.category__count}>1</span>
-                </button>
-            </div>
+      <h1>My work</h1>
+      <h3>Projects</h3>
+      <div className={style.work__categories}>
+        <button className={classNames(style.category__btn , selectBtn == 'all' ? style.selected : null)}
+        onClick={()=>{setSelectBtn('all')}}>
+          All <span className={style.category__count}>8</span>
+        </button>
+        <button className={classNames(style.category__btn , selectBtn == 'front-end' ? style.selected : null)}
+        onClick={()=>{setSelectBtn('front-end')}}>
+          Front-end <span className={style.category__count}>3</span>
+        </button>
+        <button className={classNames(style.category__btn , selectBtn == 'back-end' ? style.selected : null)}
+        onClick={()=>{setSelectBtn('back-end')}}>
+          Back-end <span className={style.category__count}>4</span>
+        </button>
+        <button className={classNames(style.category__btn , selectBtn == 'mobile' ? style.selected : null)}
+        onClick={()=>{setSelectBtn('mobile')}}>
+          Mobile <span className={style.category__count}>1</span>
+        </button>
+      </div>
 
-            <div className={style.work__projects}>
-                <a
-                    href="https://dhtube.netlify.app/"
-                    target="_blank"
-                    className={style.project}
-                    data-type="front-end"
-                >
-                    <img
-                        src={DHtube}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>React</h3>
-                        <span>DHtube</span>
-                    </div>
-                </a>
-                <a
-                    href="http://124.5.187.217:9090/"
-                    target="_blank"
-                    className={style.project}
-                    data-type="back-end"
-                >
-                    <img
-                        src={corona}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>Spring Boot</h3>
-                        <span>Corona-114</span>
-                    </div>
-                </a>
-                <a
-                    href="https://github.com/louis-25/FinalProject-raspberry"
-                    target="_blank"
-                    className={style.project}
-                    data-type="back-end"
-                >
-                    <img
-                        src="https://github.com/louis-25/FinalProject-raspberry/raw/main/md-images/image-20210615104043936.png"
-                        alt="기가지니 이미지"
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>Python</h3>
-                        <span>GiGA Genie</span>
-                    </div>
-                </a>
-                <a
-                    href="https://louis-25.github.io/React.js_Study/"
-                    target="_blank"
-                    className={style.project}
-                    data-type="front-end"
-                >
-                    <img
-                        src={TodoList}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>React.js</h3>
-                        <span>TodoList</span>
-                    </div>
-                </a>
-                <a
-                    href="https://louis-25.github.io/Clone_Coding/"
-                    target="_blank"
-                    className={style.project}
-                    data-type="front-end"
-                >
-                    <img
-                        src={github}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>HTML</h3>
-                        <span>GitHub clone-coding</span>
-                    </div>
-                </a>                
-                <a
-                    href="https://github.com/louis-25/Django_Practice/tree/django_shopping"
-                    target="_blank"
-                    className={style.project}
-                    data-type="back-end"
-                >
-                    <img
-                        src={Django}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>Django</h3>
-                        <span>쇼핑몰 관리자페이지</span>
-                    </div>
-                </a>
-                <a
-                    href="https://github.com/louis-25/TIL/tree/main/spring/SemiProject"
-                    target="_blank"
-                    className={style.project}
-                    data-type="back-end"
-                >
-                    <img
-                        src={자취꿀팁}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>Spring</h3>
-                        <span>자취꿀팁</span>
-                    </div>
-                </a>
-                <a
-                    href="https://drive.google.com/file/d/1SC4T2m8gadtkg33AoS_5UI04_95urheK/view?usp=sharing"
-                    target="_blank"
-                    className={style.project}
-                    data-type="mobile"
-                >
-                    <img
-                        src={너굴앱}
-                        alt=""
-                        className={style.project__img}
-                        width="300rem"
-                    />
-                    <div className={style.project__description}>
-                        <h3>안드로이드</h3>
-                        <span>너굴앱</span>
-                    </div>
-                </a>
-            </div>
-        </section>
+      <div className={style.work__projects}>
+        { works.map((work)=>{            
+            if(selectBtn == work.datatype || selectBtn == 'all') {
+              return(              
+                <WorkList work={work}></WorkList>
+              )
+            }
+          })        
+        }
+      </div>
+    </section>
   );
+}
+
+function WorkList({work}) {  
+  return (
+    <>     
+      <a
+        href={work.href}
+        target="_blank"
+        className={style.project}
+        data-type={work.datatype}        
+      >
+        <img
+          src={work.src}
+          alt=""
+          className={style.project__img}
+          width="300rem"
+        />
+        <div className={style.project__description}>
+          <h3>{work.h3}</h3>
+          <span>{work.span}</span>
+        </div>
+      </a>      
+    </>
+  )
 }
 
 export default Work;
