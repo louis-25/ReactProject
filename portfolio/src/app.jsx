0c8e4a9a34cebb30a1191eb733dc5dd9d2352f00
louis-25 <20151577@vision.hoseo.edu>
2021-09-27 14:57:47 +0900

    throttle 스크롤 적용

--- portfolio/src/app.jsx
+++ portfolio/src/app.jsx
@@ -6,36 +6,38 @@ import Skills from './components/skills/skills'
 import Work from './components/work/work';
 import Contact from './components/contact/contact'
 import { throttle } from 'lodash';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 
-function App() {    
+function App() {
   const [scrollY, setScrollY] = useState() //스크롤 변수
+  const home = useRef();
+
   const throttledScroll = useMemo(() =>
       throttle(() => {
-        console.log('스크롤 이벤트');      
+        console.log('스크롤 이벤트');
         console.log('y ',window.scrollY);
-        setScrollY(window.scrollY)    
-      }, 300),    
+        setScrollY(window.scrollY)
+      }, 300),
   );
 
   useEffect(() => {
     window.addEventListener('scroll', throttledScroll);
+    console.log('home ',home)
     return () => {
-      window.removeEventListener('scroll', throttledScroll);      
+      window.removeEventListener('scroll', throttledScroll);
     };
-  }, [throttledScroll]);      
+  }, []);
 
   return (
-    <>                  
+    <>
       <Navbar scrollY={scrollY} setScrollY={setScrollY}/>
-      <Home setScrollY={setScrollY}/>      
+      <Home setScrollY={setScrollY} ref={home}/>
       <About/>
       <Skills/>
       <Work/>
-      <Contact/>      
+      <Contact/>
     </>
-
   );
 }
 
