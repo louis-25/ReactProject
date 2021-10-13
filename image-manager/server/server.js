cb1d0bb5c1684d2317af9f539ac71c7d79fd4bed
louis-25 <20151577@vision.hoseo.edu>
2021-10-13 10:05:25 +0900

    uuid 설정

--- image-manager/server/server.js
+++ image-manager/server/server.js
@@ -1,11 +1,21 @@
-const express =require('express')
+const express =require('express');
+const multer = require('multer');
+const { v4: uuid } = require('uuid');
+const mime = require('mime-types')
+console.log('uuid: ', uuid());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+})
+const upload = multer({ storage })
 
 const app = express();
 const PORT = 5000;
 
-app.post('/upload', (req, res) => {
-  console.log('/upload called')
-  res.json({result: "success"})
+app.post('/upload', upload.single("imageTest"), (req, res) => {
+  console.log(req.file)  
+  res.json(req.file)
 })
 
 app.listen(PORT, () => console.log(PORT+"번 포트 listen 중~"))
