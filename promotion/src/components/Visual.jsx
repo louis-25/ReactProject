85266b7dd61a694a0266f7c0ccb35c9ef5e4e24b
louis-25 <20151577@vision.hoseo.edu>
2021-11-08 10:58:05 +0900

    [ADD] visual 반응형추가

--- promotion/src/components/Visual.jsx
+++ promotion/src/components/Visual.jsx
@@ -13,27 +13,30 @@ function Visual({promotionRef}) {
       inline: 'nearest'
     })    
   }
-  return (
-    <section className="visual">
-      <div className="inner">
-        <div className="visual-contents-box">
+  return (    
+    <section className="visual">      
+      <div className={`visual-contents-box inner`}>
         <div className="visual-contents-left">
           <h1 className="visual-main-title">안전한 콘텐츠 공유<br/>업무 메신저</h1>
           <p className="visual-text-contents">
             우리는 이제 협업 과정에서 중요 자료를 안정하게 보호<br/>
             하면서 효율적인 협업 환경을 만들 수 있는 사내 메신저<br/>
             가 반드시 필요합니다
-          </p>
-          <input className="visual-btn" onClick={goToPromotion} type="button" value="체험 신청"></input>
+          </p>          
+          <div className="visual-btn-box">
+            <input className="visual-btn" onClick={goToPromotion} type="button" value="체험 신청"></input>
+          </div>
         </div>
         <div className="visual-contents-right">
           <div className="img-box">
             <img src={Contents}/>          
           </div>
+        </div>        
+        <div className="visual-btn-box2">
+          <input className="visual-btn" onClick={goToPromotion} type="button" value="체험 신청"></input>
         </div>
-        </div>
-      </div>
-    </section>
+      </div>                          
+    </section>                
   );
 }
 
