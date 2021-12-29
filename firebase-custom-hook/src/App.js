8b8999aa6b5fc87d16e8267bf165e256db463e58
louis-25 <20151577@vision.hoseo.edu>
2021-12-29 15:55:37 +0900

    [ADD] custom-hook

---
+++ firebase-custom-hook/src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {  
+  const [tasks, setTasks] = useState([]);
+  const transformTask = tasksObj => {
+    const loadedTasks = [];
+
+    for(const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text});
+    }
+    setTasks(loadedTasks)
+  }
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp(
+      {url:'https://react-http-6b4a6.firebaseio.com/tasks.json'},
+      transformTask
+    )
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
