8689f273fcba649a081064777e85b52e4acf7aa6
louis-25 <20151577@vision.hoseo.edu>
2022-02-02 17:01:00 +0900

    [ADD] 회원가입 form 추가

--- image-manager/client/src/pages/RegisterPage.jsx
+++ image-manager/client/src/pages/RegisterPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CustomInput from '../components/CustomInput';
 
 function RegisterPage(props) {
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordCheck, setPasswordCheck] = useState("")
   return (
     <div>
-      <h1>Register</h1>
+      <div style={{
+        marginTop: 100,
+        maxWidth: 350,
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}></div>
+      <h3>회원가입</h3>
+      <form>
+        <CustomInput label="이름" value={name} setValue={setName}></CustomInput>
+        <CustomInput label="회원ID" value={username} setValue={setUsername}></CustomInput>
+        <CustomInput label="비밀번호" value={password} setValue={setPassword}></CustomInput>
+        <CustomInput 
+          label="비밀번호 확인" 
+          value={passwordCheck} 
+          setValue={setPasswordCheck}
+          type="password"
+        ></CustomInput>
+      </form>
     </div>
   );
 }
