abdd76bde5ac6ed6af6f3f42a96b2cb69562e886
louis-25 <20151577@vision.hoseo.edu>
2022-02-04 00:30:55 +0900

    [ADD] 좋아요 API 추가

--- image-manager/server/models/image.js
+++ image-manager/server/models/image.js
@@ -8,6 +8,7 @@ const ImageSchema = new mongoose.Schema(
       name: {type:String, required: true},
       username: {type: String, required: true}
     },
+    likes: [{type: mongoose.Types.ObjectId}],
     public: {type: Boolean, required: true, default: false}, // 기본값: false(비공개)
     key: { type: String, required: true },
     originalFileName: { type: String, required: true },
--- image-manager/server/routes/imageRouter.js
+++ image-manager/server/routes/imageRouter.js
@@ -44,9 +44,12 @@ imageRouter.delete("/:imageId", async (req, res) => {
     console.log(req.params)
     if(!req.user) throw new Error("권한이 없습니다.")
     if(!mongoose.isValidObjectId(req.params.imageId)) // DB에 없는 imageid 값 호출시
-      throw new Error("올바르지 않은 이미지 id입니다.")
+      throw new Error("올바르지 않은 imageId입니다.")
 
-    const image = await Image.findOneAndDelete({_id: req.params.imageId}); // id값을 찾아서 삭제
+    const image = await Image.findOneAndDelete({_id: req.params.imageId}); // id값을 DB에서 찾아서 삭제
+    if(!image) 
+      return res.json({message: "요청하싱 사진은 이미 삭제되었습니다"})
+
     await fileUnlink(`./uploads/${image.key}`) // uploads폴더에 있는 사진 데이터 삭제    
     res.json({message: "요청하신 이미지가 삭제되었습니다.", image})    
   } catch(e) {
@@ -55,14 +58,42 @@ imageRouter.delete("/:imageId", async (req, res) => {
   }
 })
 
-imageRouter.patch("/:imageId/like", (req, res) => {
+imageRouter.patch("/:imageId/like", async (req, res) => {
   // 유저 권한 확인
   // like 중복 안되도록 확인
+  try {
+    if(!req.user) throw new Error("권한이 없습니다.")
+    if(!mongoose.isValidObjectId(req.params.imageId)) // DB에 없는 imageid 값 호출시
+      throw new Error("올바르지 않은 imageId입니다.")
+    // 첫번째- 찾을값 , 두번째 - 업데이트, 세번째 - 옵션
+    const image = await Image.findOneAndUpdate(
+      {_id: req.params.imageId},
+      {$addToSet: {likes: req.user.id}}, // $addToSet - $push하려는 값이 배열에 없을때만 $push(따로 Validation 안해줘도됨)
+      {new: true} //new: true - 업데이트 된 문서를 반환
+    ) 
+    res.json(image)
+  } catch(e) {
+    console.log(e)
+    res.status(400).json({message: e.message})
+  }
 })
 
-imageRouter.patch("/:imageId/unlike", (req, res) => {
+imageRouter.patch("/:imageId/unlike", async (req, res) => {
   // 유저 권한 확인
   // like 중복 취소 안되도록 확인
+  try {
+    if(!req.user) throw new Error("권한이 없습니다.")
+    if(!mongoose.isValidObjectId(req.params.imageId)) // DB에 없는 imageid 값 호출시
+      throw new Error("올바르지 않은 imageId입니다.")
+      const image = await Image.findOneAndUpdate(
+        {_id: req.params.imageId},
+        {$pull: {likes: req.user.id}}, // $pull - 배열에서 삭제
+        {new: true} //new: true - 업데이트 된 문서를 반환
+      ) 
+  } catch(e) {
+    console.log(e)
+    res.status(400).json({message: e.message})
+  }
 })
 
 module.exports={imageRouter}
