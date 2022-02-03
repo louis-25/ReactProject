1115034e9c6e70ce8d36546ed09ac48f8660551d
louis-25 <20151577@vision.hoseo.edu>
2022-02-04 00:03:19 +0900

    [ADD] 이미지 삭제 API 추가

--- image-manager/server/routes/imageRouter.js
+++ image-manager/server/routes/imageRouter.js
@@ -2,6 +2,11 @@ const { Router } = require("express")
 const imageRouter = Router()
 const Image = require("../models/image");
 const { upload } = require("../middleware/imageUpload")
+const fs = require("fs")
+const mongoose = require("mongoose")
+const { promisify } = require("util") // 프로미스화 시켜주는 라이브러리
+
+const fileUnlink = promisify(fs.unlink) // fs.unlink - 파일삭제 기능
 
 imageRouter.post('/', upload.single("image"), async (req, res) => {
   // 유저 정보, public 유무 확인
@@ -30,9 +35,24 @@ imageRouter.get("/", async (req, res) => {
   res.json(images);
 });
 
-imageRouter.delete("/:imageId", (req, res) => {
+imageRouter.delete("/:imageId", async (req, res) => {
   // 유저 권한 확인
   // 사진 삭제
+  // 1. uploads 폴더에 있는 사진 데이터를 삭제
+  // 2. 데이터베이스에 있는 image 문서를 삭제
+  try {
+    console.log(req.params)
+    if(!req.user) throw new Error("권한이 없습니다.")
+    if(!mongoose.isValidObjectId(req.params.imageId)) // DB에 없는 imageid 값 호출시
+      throw new Error("올바르지 않은 이미지 id입니다.")
+
+    const image = await Image.findOneAndDelete({_id: req.params.imageId}); // id값을 찾아서 삭제
+    await fileUnlink(`./uploads/${image.key}`) // uploads폴더에 있는 사진 데이터 삭제    
+    res.json({message: "요청하신 이미지가 삭제되었습니다.", image})    
+  } catch(e) {
+    console.log(e)
+    res.status(400).json({message: e.message})
+  }
 })
 
 imageRouter.patch("/:imageId/like", (req, res) => {
