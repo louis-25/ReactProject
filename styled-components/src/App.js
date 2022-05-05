53b80baa920cbcd8eaad061a641e470f6c7a5660
louis-25 <20151577@vision.hoseo.edu>
2022-05-05 13:51:20 +0900

    attrs속성 사용해보기

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -4,17 +4,14 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Father>
-      <Btn>Log in</Btn>
+      <Input></Input>
     </Father>
   );
 }
