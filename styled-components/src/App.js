85ae98d5e45614ac7b424325d2ef46b36cde42e3
louis-25 <20151577@vision.hoseo.edu>
2022-05-05 13:44:52 +0900

    as속성 사용해보기

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -4,21 +4,17 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal"></Box>
-      <Circle bgColor="tomato"></Circle>
+      <Btn>Log in</Btn>
     </Father>
   );
 }
