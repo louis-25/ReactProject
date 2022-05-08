c40c742969276b9e92e1a23494b41ca9a62799ed
louis-25 <20151577@vision.hoseo.edu>
2022-05-08 21:26:19 +0900

    Route States값 관리

--- coin-tracker/src/routes/Coin.tsx
+++ coin-tracker/src/routes/Coin.tsx
@@ -1,12 +1,63 @@
-import React, { FunctionComponent } from "react";
-import { useParams } from "react-router-dom";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface CoinsProps {}
+const Container = styled.div`
+  padding: 0px 20px;
+  width: 500px;
+  margin: auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteState {
+  state: {
+    name: string;
+  };
+}
 
 const Coin: FunctionComponent<CoinsProps> = ({}) => {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
-
-  return <div>Coin {coinId}</div>;
+  const { state } = useLocation() as RouteState;
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coin {state?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 };
 
 export default Coin;
--- coin-tracker/src/routes/Coins.tsx
+++ coin-tracker/src/routes/Coins.tsx
@@ -6,10 +6,12 @@ interface CoinsProps {}
 
 const Container = styled.div`
   padding: 0px 20px;
+  width: 500px;
+  margin: auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -23,9 +25,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     transition: color 0.2s ease-in;
     padding: 20px;
-    display: block;
   }
   &:hover {
     a {
@@ -35,12 +38,19 @@ const Coin = styled.li`
 `;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -76,7 +86,15 @@ const Coins: FunctionComponent<CoinsProps> = ({}) => {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={`/${coin.id}`}
+                state={{ name: coin.name, rank: coin.rank }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                ></Img>
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
