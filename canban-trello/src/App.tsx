6ee1f01459bda50a2ab3b49d3ad9fb154e24faec
louis-25 <20151577@vision.hoseo.edu>
2022-07-17 15:07:06 +0900

    theme setting

--- canban-trello/src/App.tsx
+++ canban-trello/src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { hourSelector, minuteState } from './atoms'
-import './App.css'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${props => props.theme.cardColor};
+`
+
+const toDos = ['a', 'b', 'c', 'd', 'e', 'f']
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState)
-  const hours = useRecoilValue(hourSelector)
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value)
-  }
+  const onDragEnd = () => {}
   return (
-    <div className="App">
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input value={hours} type="number" placeholder="Hours" />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {provided => (
+              <Board ref={provided.innerRef} {...provided.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {provided => (
+                      <Card
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
   )
 }
 
